310cb3cc8d7945f140b979383c5b7bc6a2432d80
kloudkitsune <corbansmith92@gmail.com>
2025-09-03 21:14:41 -0600

    added functions for add and remove Modal to remove repetitive code function, updated modal.css with updated font styles and padding for mobile view.

--- scripts/index.js
+++ scripts/index.js
@@ -21,36 +21,46 @@ const profileDescriptionEl = document.querySelector(".profile__description");
 const newPostImageInput = document.querySelector("#card-image-input"); //card image input
 const newPostCaptionInput = document.querySelector("#card-caption-input"); //card caption input
 
+//function for opening and closing modals
+function openModal(modal) {
+  modal.classList.add("modal_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("modal_is-opened");
+}
+
 editProfileBtn.addEventListener("click", function () {
   editProfileNameInput.value = profileNameEl.textContent;
   editProfileDescriptionInput.value = profileDescriptionEl.textContent;
-  editProfileModal.classList.add("modal_is-opened");
+  openModal(editProfileModal);
 });
 
 editProfileCloseBtn.addEventListener("click", function () {
-  editProfileModal.classList.remove("modal_is-opened");
+  closeModal(editProfileModal);
 });
 
 newPostBtn.addEventListener("click", function () {
-  newPostModal.classList.add("modal_is-opened");
+  openModal(newPostModal);
 });
 
 newPostCloseBtn.addEventListener("click", function () {
-  newPostModal.classList.remove("modal_is-opened");
+  closeModal(newPostModal);
 });
 
 function handleAddCardSubmit(evt) {
   evt.preventDefault();
   console.log("Image URL:", newPostImageInput.value);
   console.log("Caption:", newPostCaptionInput.value);
-  newPostModal.classList.remove("modal_is-opened"); // New post function
+  closeModal(newPostModal); // New post function
+  evt.target.reset();
 }
 
 function handleEditProfileSubmit(evt) {
   evt.preventDefault();
   profileNameEl.textContent = editProfileNameInput.value;
   profileDescriptionEl.textContent = editProfileDescriptionInput.value;
-  editProfileModal.classList.remove("modal_is-opened");
+  closeModal(editProfileModal);
 }
 
 editProfileForm.addEventListener("submit", handleEditProfileSubmit);
